9150f1c9f8942626ef7ce5f72636c939cee1c4ed
André Siqueira <siqueira.andre@gmail.com>
2020-07-19 17:35:06 -0300

    Library OK

--- _app/Email.php
+++ _app/Email.php
@@ -7,22 +7,22 @@ class Email
 {
     private $mail = \stdClass::class;
 
-    public function __construct()
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName)
     {
         //Server settings
         $this->mail = new PHPMailer(true);
-        $this->mail->SMTPDebug = 2;                                       // Enable verbose debug output
+        $this->mail->SMTPDebug = $smtpDebug;                              // Enable verbose debug output
         $this->mail->isSMTP();                                            // Send using SMTP
-        $this->mail->Host       = 'smtp1.example.com';                    // Set the SMTP server to send through
+        $this->mail->Host       = $host;                                  // Set the SMTP server to send through
         $this->mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $this->mail->Username   = 'user@example.com';                     // SMTP username
-        $this->mail->Password   = 'secret';                               // SMTP password
-        $this->mail->SMTPSecure = 'tls';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $this->mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $this->mail->Username   = $user;                                  // SMTP username
+        $this->mail->Password   = $pass;                                  // SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;                            // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $this->mail->Port       = $port;                                  // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         $this->mail->CharSet    = 'utf-8';
         $this->mail->SetLanguage('br');
         $this->mail->isHTML(true);
-        $this->mail->setFrom('from@example.com', 'Mailer');
+        $this->mail->setFrom($setFromEmail, $setFromName);
 
     }
 
--- test/index.php
+++ test/index.php
@@ -1,10 +1,11 @@
 <?php
 
-require __DIR__ . '/lib_ext/autoload.php';
+require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email;
+$novoEmail = new Email(2,'smtp1.example.com', 'user@exemple.com','secret','tls',587,
+    'from@exemple.com','Mailer');
 $novoEmail->sendMail("Assunto de Teste", "<p>Esse é um e-mail de <b>teste!</b></p>","siqueira.andre@gmail.com", "André Siqueira");
 
 var_dump($novoEmail);
